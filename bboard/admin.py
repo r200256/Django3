ff732dd8d5ef00904d671cc5950cb0a12959e72b
WORK <work@work>
2023-03-02 17:04:42 +0600

    Day 2

--- bboard/admin.py
+++ bboard/admin.py
@@ -4,9 +4,12 @@ from django.contrib import admin
 
 from .models import Bb
 
+from .models import Rubric
+admin.site.register(Rubric)
+
 
 class BbAdmin(admin.ModelAdmin):
-    list_display = ("title", "content", "price", "published")
+    list_display = ("title", "content", "price", "published", "rubric")
     list_display_links = ("title", "content", "price")
     search_fields = ("title", "content",)
 
--- bboard/views.py
+++ bboard/views.py
@@ -1,12 +1,25 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 
 from .models import Bb
+from .models import Rubric
 
 
 def index(request):
-    template = loader.get_template('bboard/index.html')
-    bbs = Bb.all()
-    context = {"bbs": bbs}
-    return HttpResponse(template.render(context, request))
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs,
+               'rubrics': rubrics,
+               'current_rubric': current_rubric}
+    return render(request,
+                  'bboard/by_rubric.html', context)
